6c9816ed1108743d2aecd6808abb5a463fd7a27b
BONINI FRANCESCO s282890 <s282890@studenti.polito.it>
2022-03-10 12:49:57 +0100

    Aggiunto prototipo schermata principale

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Col, Row, Container, Image } from 'react-bootstrap';
+import AppSidebar from "./components/AppSidebar";
+import { AppNavbar } from './components/AppNavbar';
+
+import proPic from './components/Images/myPhoto.jpg' 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Testing app</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className="App m-0 p-0">
+      <div id="body" class="pt-3 px-3">
+      <AppNavbar/>
+        <Row>
+          <Col md={2}>
+            <AppSidebar/>
+          </Col>
+          <Col md={10}>
+            <Image inline className="mx-auto d-none d-sm-block" src={proPic} roundedCircle width = "200" />
+            <div className = "mt-3 pt-5">
+              <p>Via silvio pellico 1</p>
+              <p>Via silvio pellico 1</p>
+            </div>
+
+            <div className = "mt-3 pt-5 d-flex">
+              <h2>Su di me</h2>
+            </div>
+
+
+            <div className = "mt-3 pt-5 d-flex">
+              <h2>Mie Passioni</h2>
+            </div>
+            
+            
+          </Col>
+        </Row>
+        
+      </div>
+    </Container>
   );
 }
 
---
+++ src/components/AppSidebar.js
@@ -0,0 +1,38 @@
+import "./AppSidebar.css";
+import { Nav, ListGroup, NavLink } from "react-bootstrap";
+import {
+  AboutMeBtn,
+  SkillsBtn,
+  PortfolioBtn,
+  StrudentCardBtn,
+  SocialsBtn,
+} from "./SideBarButton";
+
+function AppSidebar() {
+  return (
+    <div>
+      <Nav id="filters-nav">
+        <ListGroup as="ul" id="filters-nav-list">
+          
+          <NavLink as="li" className="myContainer">
+            <AboutMeBtn />
+          </NavLink>
+          <NavLink as="li" className="myContainer">
+            <SkillsBtn />
+          </NavLink>
+          <NavLink as="li" className="myContainer">
+            <PortfolioBtn />
+          </NavLink>
+          <NavLink as="li" className="myContainer">
+            <StrudentCardBtn />
+          </NavLink>
+          <NavLink as="li" className="myContainer">
+            <SocialsBtn />
+          </NavLink>
+        </ListGroup>
+      </Nav>
+    </div>
+  );
+}
+
+export default AppSidebar;
