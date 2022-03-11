5c626ff887390b5c399e91f2b434907a4c97c5e7
BONINI FRANCESCO s282890 <s282890@studenti.polito.it>
2022-03-11 19:52:13 +0100

    bug-fix

--- src/components/SideBarButton.js
+++ src/components/SideBarButton.js
@@ -1,5 +1,5 @@
 import "./SideBarButton.css";
-import { Button } from "react-bootstrap";
+import { Button, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { Link } from 'react-router-dom';
 
 
@@ -8,6 +8,14 @@ function AboutMeBtn(props) {
   console.log(props.title);
   return (
     <>
+      <OverlayTrigger
+        delay={{ hide: 450, show: 300 }}
+        overlay={(props) => (
+          <Tooltip {...props}>
+            Su di Me
+          </Tooltip>
+        )}
+        placement="right">
       <Link to="/"><Button variant={props.title === "AboutMe" ?  "dark" : "outline-dark"} id="add-survey-btn">
         <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-fingerprint" viewBox="0 0 16 16">
           <path d="M8.06 6.5a.5.5 0 0 1 .5.5v.776a11.5 11.5 0 0 1-.552 3.519l-1.331 4.14a.5.5 0 0 1-.952-.305l1.33-4.141a10.5 10.5 0 0 0 .504-3.213V7a.5.5 0 0 1 .5-.5Z" />
@@ -16,7 +24,7 @@ function AboutMeBtn(props) {
           <path d="M3.902 4.222a4.996 4.996 0 0 1 5.202-2.113.5.5 0 0 1-.208.979 3.996 3.996 0 0 0-4.163 1.69.5.5 0 0 1-.831-.556Zm6.72-.955a.5.5 0 0 1 .705-.052A4.99 4.99 0 0 1 13.059 7v1.5a.5.5 0 1 1-1 0V7a3.99 3.99 0 0 0-1.386-3.028.5.5 0 0 1-.051-.705ZM3.68 5.842a.5.5 0 0 1 .422.568c-.029.192-.044.39-.044.59 0 .71-.1 1.417-.298 2.1l-1.14 3.923a.5.5 0 1 1-.96-.279L2.8 8.821A6.531 6.531 0 0 0 3.058 7c0-.25.019-.496.054-.736a.5.5 0 0 1 .568-.422Zm8.882 3.66a.5.5 0 0 1 .456.54c-.084 1-.298 1.986-.64 2.934l-.744 2.068a.5.5 0 0 1-.941-.338l.745-2.07a10.51 10.51 0 0 0 .584-2.678.5.5 0 0 1 .54-.456Z" />
           <path d="M4.81 1.37A6.5 6.5 0 0 1 14.56 7a.5.5 0 1 1-1 0 5.5 5.5 0 0 0-8.25-4.765.5.5 0 0 1-.5-.865Zm-.89 1.257a.5.5 0 0 1 .04.706A5.478 5.478 0 0 0 2.56 7a.5.5 0 0 1-1 0c0-1.664.626-3.184 1.655-4.333a.5.5 0 0 1 .706-.04ZM1.915 8.02a.5.5 0 0 1 .346.616l-.779 2.767a.5.5 0 1 1-.962-.27l.778-2.767a.5.5 0 0 1 .617-.346Zm12.15.481a.5.5 0 0 1 .49.51c-.03 1.499-.161 3.025-.727 4.533l-.07.187a.5.5 0 0 1-.936-.351l.07-.187c.506-1.35.634-2.74.663-4.202a.5.5 0 0 1 .51-.49Z" />
         </svg>
-      </Button></Link>
+      </Button></Link></OverlayTrigger>
     </>
   );
 }
@@ -24,6 +32,14 @@ function AboutMeBtn(props) {
 function SkillsBtn(props) {
   return (
     <>
+      <OverlayTrigger
+        delay={{ hide: 450, show: 300 }}
+        overlay={(props) => (
+          <Tooltip {...props}>
+            Skills
+          </Tooltip>
+        )}
+        placement="right">
       <Link to="/Skills"><Button variant={props.title === "Skills" ?  "dark" : "outline-dark"} id="add-survey-btn">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -36,7 +52,7 @@ function SkillsBtn(props) {
           <path d="M10 2a2 2 0 0 1-1.5 1.937v5.087c.863.083 1.5.377 1.5.726 0 .414-.895.75-2 .75s-2-.336-2-.75c0-.35.637-.643 1.5-.726V3.937A2 2 0 1 1 10 2z" />
           <path d="M0 9.665v1.717a1 1 0 0 0 .553.894l6.553 3.277a2 2 0 0 0 1.788 0l6.553-3.277a1 1 0 0 0 .553-.894V9.665c0-.1-.06-.19-.152-.23L9.5 6.715v.993l5.227 2.178a.125.125 0 0 1 .001.23l-5.94 2.546a2 2 0 0 1-1.576 0l-5.94-2.546a.125.125 0 0 1 .001-.23L6.5 7.708l-.013-.988L.152 9.435a.25.25 0 0 0-.152.23z" />
         </svg>
-      </Button></Link>
+      </Button></Link></OverlayTrigger>
     </>
   );
 }
@@ -44,6 +60,14 @@ function SkillsBtn(props) {
 function PortfolioBtn(props) {
   return (
     <>
+    <OverlayTrigger
+        delay={{ hide: 450, show: 300 }}
+        overlay={(props) => (
+          <Tooltip {...props}>
+            Portfolio
+          </Tooltip>
+        )}
+        placement="right">
       <Link to="/Portfolio"><Button variant={props.title === "Portfolio" ?  "dark" : "outline-dark"} id="add-survey-btn">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -55,7 +79,7 @@ function PortfolioBtn(props) {
         >
           <path d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4H2.19zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707z" />
         </svg>
-      </Button></Link>
+      </Button></Link></OverlayTrigger>
     </>
   );
 }
@@ -63,6 +87,14 @@ function PortfolioBtn(props) {
 function StudentCardBtn(props) {
   return (
     <>
+    <OverlayTrigger
+        delay={{ hide: 450, show: 300 }}
+        overlay={(props) => (
+          <Tooltip {...props}>
+            Libretto studente
+          </Tooltip>
+        )}
+        placement="right">
       <Link to="/StudentCard"><Button variant={props.title === "StudentCard" ?  "dark" : "outline-dark"} id="add-survey-btn">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -75,7 +107,7 @@ function StudentCardBtn(props) {
           <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5ZM8 8.46 1.758 5.965 8 3.052l6.242 2.913L8 8.46Z" />
           <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Zm-.068 1.873.22-.748 3.496 1.311a.5.5 0 0 0 .352 0l3.496-1.311.22.748L8 12.46l-3.892-1.556Z" />
         </svg>
-      </Button></Link>
+      </Button></Link></OverlayTrigger>
     </>
   );
 }
@@ -83,6 +115,14 @@ function StudentCardBtn(props) {
 function SocialsBtn(props) {
   return (
     <>
+    <OverlayTrigger
+        delay={{ hide: 450, show: 300 }}
+        overlay={(props) => (
+          <Tooltip {...props}>
+            Socials
+          </Tooltip>
+        )}
+        placement="right">
       <Link to="/Socials"><Button variant={props.title === "Socials" ?  "dark" : "outline-dark"} id="add-survey-btn">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -94,7 +134,7 @@ function SocialsBtn(props) {
         >
           <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z" />
         </svg>
-      </Button></Link>
+      </Button></Link></OverlayTrigger>
     </>
   );
 }
