1609d6226741c1e3d37aad7257161abe739d929b
BONINI FRANCESCO s282890 <s282890@studenti.polito.it>
2022-03-11 11:38:32 +0100

    Aggiunto Portfolio con cards

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,32 +1,73 @@
-const skills = [
-  {name : "GRAFICA", list : "Unity | Blender | OpenGL"},
-  {name : "LINGUAGGI DI PROGRAMMAZIONE", list : "Assembly | C | C++ | C# | Java | Javascript | Html | Python"},
-  {name : "ANALISI E VISUALIZZAZIONE DATI", list : "Sistemi di gestione di database (DBMS) | SQL | MATLAB"},
-  {name : "SISTEMI OPERATIVI", list : "Windows | Linux | Unix based systems"},
-  {name : "CRITTOGRAFIA E SICUREZZA INFORMATICA", list : "Python | C"},
-  {name : "OFFICE AUTOMATION", list : "Elaborazione testi | Fogli elettronici | Software di presentazione | Suite da ufficio | Web Browser"},
-  {name : "ELABORAZIONE FOTO E VIDEO", list : "Photoshop | DaVinci | GIMP"},
-  {name : "ARCHITETTURE DI RETE", list : "Wireshark | Ettercap"} 
-]  
+import { Card, Placeholder, Button } from "react-bootstrap";
+import { Image } from "react-bootstrap";
+import pr0 from "./Images/Portfolio/IG_blender.jpg";
+import pr1 from "./Images/Portfolio/IG_openGL.jpg";
+import pr2 from "./Images/Portfolio/RV.jpg";
+import pr3 from "./Images/Portfolio/WEBAPP.jpg";
+import pr4 from "./Images/Portfolio/ASE.jpg";
+import pr5 from "./Images/Portfolio/SWENG.png";
+
+const projects = [
+  {propic: pr0, data: "Mar 2022", nome: "Informatica Grafica: Blender", breveDescrizione: "Modellazione ed il rendering di scene 3D statiche in Blender", page : true},
+  {propic: pr1, data: "Mar 2022", nome: "Informatica Grafica: OpenGL", breveDescrizione: "Realizzazione di applicazione interattiva in openGL", page : true},
+  {propic: pr2, data: "Feb 2022", nome: "Realtà Virtuale", breveDescrizione: "Applicazione desktop di natura didattica per scoprire il complesso e affascinante mondo delle formiche a partire dal loro punto di vista", page : true},
+  {propic: pr3, data: "Jul 2021", nome: "Applicazione Web", breveDescrizione: "Sito di gestione di Surveys (Questionari) in React Bootstrap, gestendo sia parte di fontend sia parte backend", page : true},
+  {propic: pr4, data: "Jan 2021", nome: "Architetture degli elaboratori", breveDescrizione: "Applicazione interattiva per emulatore Keil uVision della scheda LandTiger", page : true},
+  {propic: pr5, data: "Jun 2021", nome: "Progetto di ingegneria del Software", breveDescrizione: "Ezshop: applicazione per piccoli negozi per supportare proprietario e manager", page : false}
+]
 
 function PortfolioPage(props) {
   props.updateTitle("Portfolio");
   return (
     <div className="mt-5 pt-4">
-      {skills.map((s) => <SkillField name={s.name} list={s.list}/>)}      
-    </div>        
+      <div className="d-flex flex-wrap justify-content-around">
+        {projects.map((p) => <AddProject project={p}/>)}
+        <div className="d-flex justify-content-around mt-5 pt-5">
+
+        <div className="m-3">
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Placeholder as={Card.Title} animation="glow">
+              <Placeholder xs={6} />
+            </Placeholder>
+            <Placeholder as={Card.Text} animation="glow">
+              <Placeholder xs={7} /> <Placeholder xs={4} />{" "}
+              <Placeholder xs={4} /> <Placeholder xs={6} />{" "}
+              <Placeholder xs={8} />
+            </Placeholder>
+            <Placeholder.Button variant="danger" xs={6}>
+              Incoming
+            </Placeholder.Button>
+          </Card.Body>
+        </Card>
+        </div>
+      </div>
+      </div>
+    </div>
   );
 }
 
 
-function SkillField(props) {
+function AddProject(props)
+{
   return(
-    <div className="mt-4">
-      <h4><strong>{props.name}</strong></h4>
-      <h6>{props.list}</h6>
-    </div>    
-  );
-  
+      <div className="m-3">
+      <small>{props.project.data}</small>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={props.project.propic} />
+          <Card.Body>
+            <Card.Title>{props.project.name}</Card.Title>
+            <Card.Text>
+            {props.project.breveDescrizione}             
+            </Card.Text>
+            <div style={{ "textAlign": "center" }}>
+              {props.project.page ? <Button variant="danger">Vai</Button> : <></>} 
+            </div>            
+          </Card.Body>
+        </Card>
+      </div>
+  )
 }
 
 export { PortfolioPage };
